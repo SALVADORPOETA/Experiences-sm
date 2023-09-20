54724129aa754f867aec181bb22219c5260d16e7
SALVADORPOETA <dsml@live.com.mx>
2023-09-20 12:04:41 -0600

    AiOutlineClose

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { HiMenuAlt3 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -15,7 +16,7 @@ const Navbar = () => {
   return (
     <div className='absolute w-full flex justify-between p-4'>
         <h1 className='text-white font-bold text-2xl z-20'>Experiences</h1>
-        <HiMenuAlt3 onClick={handleNav} className='z-20 text-white cursor-pointer' size={25} />
+        {nav ? <AiOutlineClose onClick={handleNav} className='z-20 text-white cursor-pointer' size={25} /> : <HiMenuAlt3 onClick={handleNav} className='z-20 text-white cursor-pointer' size={25} />}
         <div className={
             nav 
                 ? 'ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10' 
